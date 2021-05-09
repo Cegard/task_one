09939be4b5369fb66c0020f3a20ee1b0a6e67231
Eduardo Galeano <cegard689@gmail.com>
2021-05-09 02:01:05 -0500

    Isolated componets crashing.

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
+
+
+# changes made by Eduardo Galeano
+
+If the app crashs whe using the `npm start` command, try building it with `npm ru bild` and using a static server like `npm serve`
--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import {ErrorBoundary} from 'react-error-boundary';
+
+
+const ErrMsgComponent = ({ error }) => {
+  return (
+    <div>
+      <h1>An error occurred: {error.message}</h1>
+    </div>
+  );
+};
+
+
 class Counter extends React.Component {
+
+
   constructor(props) {
     super(props);
     this.state = { counter: 0 };
     this.handleClick = this.handleClick.bind(this);
   }
   
+
   handleClick() {
     this.setState(({counter}) => ({
       counter: counter + 1
     }));
   }
   
+
   render() {
     if (this.state.counter === 5) {
       throw new Error('Crashed.');
@@ -20,9 +36,13 @@ class Counter extends React.Component {
   }
 }
 
+
 function App() {
+  const [thereWasAnError, setError] = React.useState(false)
+
   return (
     <div>
+
       <p>
         <b>
           Click on the numbers to increase the counters.
@@ -30,9 +50,17 @@ function App() {
           The counter is programmed to throw an error when it reaches 5. This simulates a JavaScript error in a component.
         </b>
       </p>
+      
       <hr />
+
+      <ErrorBoundary FallbackComponent={ErrMsgComponent}>
         <Counter />
+      </ErrorBoundary>
+      
+      <ErrorBoundary FallbackComponent={ErrMsgComponent}>
         <Counter />
+      </ErrorBoundary>
+
       <hr />
     </div>
   );
